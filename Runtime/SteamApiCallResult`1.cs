9d79b889e55844abe109637a1ba4669547bf938f
谢凯文 <844038603@qq.com>
2026-07-25 03:56:02 +0800

    Task-based asynchronous pattern (TAP) wrapper for Steamworks async callback model.

---
+++ Runtime/SteamApiCallResult`1.cs
@@ -0,0 +1,38 @@
+﻿using Steamworks;
+
+namespace Aurora.Steam
+{
+    /// <summary>
+    /// 封装从 <see cref="CallResult{T}.APIDispatchDelegate"/> 接收到的回调结果。
+    /// </summary>
+    /// <typeparam name="TResult">调用 <c>SteamXxx.RequestXxx</c> 方法时，在该方法的文档注释中出现的回调类型。</typeparam>
+    public readonly struct SteamApiCallResult<TResult>
+    {
+        /// <summary>
+        /// Steam 回调结果。
+        /// </summary>
+        /// <remarks>当 <see cref="IOFailure"/> 为 <see langword="true"/> 时无效。</remarks>
+        public readonly TResult Result;
+
+        /// <summary>
+        /// 请求是否因传输层故障而未抵达 Steam 服务器。
+        /// </summary>
+        /// <remarks>
+        /// 为 <see langword="true"/> 时，应重试调用 Steam API。
+        /// <br/>
+        /// 可通过 <see cref="SteamUtils.GetAPICallFailureReason"/> 查询详细失败原因（但主要用于调试）。
+        /// </remarks>
+        public readonly bool IOFailure;
+
+        /// <summary>
+        /// 初始化 <see cref="SteamApiCallResult{TResult}"/> 结构的新实例。
+        /// </summary>
+        /// <param name="result">Steam 回调结果。</param>
+        /// <param name="ioFailure">请求是否因传输层故障而未抵达 Steam 服务器。</param>
+        public SteamApiCallResult(TResult result, bool ioFailure)
+        {
+            Result    = result;
+            IOFailure = ioFailure;
+        }
+    }
+}
---
+++ Runtime/Threading/Tasks/SteamTasks.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Steamworks;
+using UnityEngine.Assertions;
+
+namespace Aurora.Steam.Threading.Tasks
+{
+    /// <summary>
+    /// 提供一组返回值为 <see cref="Task"/> 或 <see cref="Task{TResult}"/> 的方法。
+    /// </summary>
+    public static class SteamTasks
+    {
+        /// <summary>
+        /// 获取一个在指定的 Steam 事件触发时完成的任务。
+        /// </summary>
+        /// <param name="cancellationToken">取消令牌。</param>
+        /// <typeparam name="TResult">你想要监听的 Steam 事件回调类型（见 <c>SteamCallbacks.cs</c> 文件）。</typeparam>
+        /// <returns>在 <typeparamref name="TResult"/> 事件触发时完成的任务。</returns>
+        public static Task<TResult> WhenCallback<TResult>(CancellationToken cancellationToken = default)
+            where TResult : struct
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled<TResult>(cancellationToken);
+            }
+            var promise = cancellationToken.CanBeCanceled switch
+            {
+                false => new CallbackPromise<TResult>(),
+                true  => new CallbackPromiseWithCancellation<TResult>(cancellationToken)
+            };
+            return promise.Task;
+        }
+
+        private class CallbackPromise<TResult> : TaskCompletionSource<TResult>
+        {
+            private readonly Callback<TResult> _callback;
+
+            internal CallbackPromise()
+            {
+                _callback = Callback<TResult>.Create(Triggered);
+            }
+
+            private void Triggered(TResult result)
+            {
+                if (TrySetResult(result))
+                {
+                    CleanUp();
+                }
+            }
+
+            protected virtual void CleanUp()
+            {
+                _callback.Dispose();
+            }
+        }
+
+        private sealed class CallbackPromiseWithCancellation<TResult> : CallbackPromise<TResult>
+        {
+            private static readonly Action<object> ActionCompleteCanceled = CompleteCanceled;
+
+            private readonly CancellationTokenRegistration _cancellationTokenRegistration;
+
+            internal CallbackPromiseWithCancellation(CancellationToken cancellationToken)
+            {
+                _cancellationTokenRegistration = cancellationToken.Register(
+                    ActionCompleteCanceled,
+                    Tuple.Create(this, cancellationToken)
+                );
+                if (Task.IsCompleted)
+                {
+                    _cancellationTokenRegistration.Dispose();
+                }
+            }
+
+            private static void CompleteCanceled(object state)
+            {
+                var (callbackPromiseWithCancellation, cancellationToken) =
+                    (Tuple<CallbackPromiseWithCancellation<TResult>, CancellationToken>) state;
+                if (callbackPromiseWithCancellation.TrySetCanceled(cancellationToken))
+                {
+                    callbackPromiseWithCancellation.CleanUp();
+                }
+            }
+
+            protected override void CleanUp()
+            {
+                _cancellationTokenRegistration.Dispose();
+                base.CleanUp();
+            }
+        }
+
+        /// <summary>
+        /// 获取一个在指定的 Steam API 调用结果返回时完成的任务。
+        /// </summary>
+        /// <param name="call">各种 <c>SteamXxx.RequestXxx</c> 方法返回的 Steam API 调用句柄。</param>
+        /// <param name="cancellationToken">取消令牌。</param>
+        /// <typeparam name="TResult">对应 Steam API 方法的回调结果类型（在 <c>SteamXxx.RequestXxx</c> 方法的文档注释里）。</typeparam>
+        /// <returns>在指定的 Steam API 调用结果返回时完成的任务。</returns>
+        public static Task<SteamApiCallResult<TResult>> WhenCallResult<TResult>(
+            SteamAPICall_t    call,
+            CancellationToken cancellationToken = default)
+        {
+            Assert.AreNotEqual(SteamAPICall_t.Invalid, call);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled<SteamApiCallResult<TResult>>(cancellationToken);
+            }
+            var promise = cancellationToken.CanBeCanceled switch
+            {
+                false => new CallResultPromise<TResult>(call),
+                true  => new CallResultPromiseWithCancellation<TResult>(call, cancellationToken)
+            };
+            return promise.Task;
+        }
+
+        private class CallResultPromise<TResult> : TaskCompletionSource<SteamApiCallResult<TResult>>
+        {
+            private readonly CallResult<TResult> _callResult;
+
+            internal CallResultPromise(SteamAPICall_t steamApiCall)
+            {
+                (_callResult = CallResult<TResult>.Create(Complete)).Set(steamApiCall);
+            }
+
+            private void Complete(TResult result, bool ioFailure)
+            {
+                if (TrySetResult(new SteamApiCallResult<TResult>(result, ioFailure)))
+                {
+                    CleanUp();
+                }
+            }
+
+            protected virtual void CleanUp()
+            {
+                _callResult.Dispose();
+            }
+        }
+
+        private sealed class CallResultPromiseWithCancellation<TResult> : CallResultPromise<TResult>
+        {
+            private static readonly Action<object> ActionCompleteCanceled = CompleteCanceled;
+
+            private readonly CancellationTokenRegistration _cancellationTokenRegistration;
+
+            internal CallResultPromiseWithCancellation(
+                SteamAPICall_t    steamApiCall,
+                CancellationToken cancellationToken) : base(steamApiCall)
+            {
+                _cancellationTokenRegistration = cancellationToken.Register(
+                    ActionCompleteCanceled,
+                    Tuple.Create(this, cancellationToken)
+                );
+                if (Task.IsCompleted)
+                {
+                    _cancellationTokenRegistration.Dispose();
+                }
+            }
+
+            private static void CompleteCanceled(object state)
+            {
+                var (callPromiseWithCancellation, cancellationToken) =
+                    (Tuple<CallResultPromiseWithCancellation<TResult>, CancellationToken>) state;
+                if (callPromiseWithCancellation.TrySetCanceled(cancellationToken))
+                {
+                    callPromiseWithCancellation.CleanUp();
+                }
+            }
+
+            protected override void CleanUp()
+            {
+                _cancellationTokenRegistration.Dispose();
+                base.CleanUp();
+            }
+        }
+    }
+}
